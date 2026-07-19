a5017a7d864768adb12c7052d5a93d3194f5ca50
shaiksufya191@gmail.com <shaiksufya191@gmail.com>
2026-07-19 13:56:03 +0530

    Merge remote changes from GitHub

---
+++ Deep_Sklling/Week_1/Module 4 -  NUnit and Moq/1. NUnit-Handson/CalculatorTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using CalcLibrary;
+
+namespace CalcLibrary.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator? _calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _calculator = new Calculator();
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            _calculator = null;
+        }
+
+        [TestCase(10, 20, 30)]
+        [TestCase(5, 5, 10)]
+        [TestCase(1, 2, 3)]
+        public void Add_ShouldReturnExpectedResult(int a, int b, int expected)
+        {
+            int actual = _calculator!.Add(a, b);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
